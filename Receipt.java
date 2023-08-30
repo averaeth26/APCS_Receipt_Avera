d77dd466611071ee3d1cea7d71639559709809e9
Ethan Avera <averaeth26@7hills.org>
2023-08-30 13:06:43 -0400

    Hi Mr. Twyford! I added magic numbers...

--- Receipt.java
+++ Receipt.java
@@ -5,6 +5,10 @@ public class Receipt{
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = scan.nextLine();
-        System.out.println("Hello, " + name + "!");
+        System.out.print("Enter your lucky number: ");
+        int num1 = scan.nextInt();
+        System.out.print("Enter your other lucky number: ");
+        int num2 = scan.nextInt();
+        System.out.println("Hello, " + name + "! Your Magic number is " + ((num1*num2)+10)*2 + "!");
     }
 }
