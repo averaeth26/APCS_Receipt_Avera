4ec5b38075c7cf017ade73a07a867c8c41916a8c
Ethan Avera <averaeth26@7hills.org>
2023-08-30 12:58:28 -0400

    Created Receipt File.

---
+++ Receipt.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Receipt{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scan.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
