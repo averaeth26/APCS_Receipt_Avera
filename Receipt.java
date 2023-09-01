373c42ea569702fbe98253eedb4ae3c34b5f1ea5
Ethan Avera <averaeth26@7hills.org>
2023-09-01 12:41:49 -0400

    Began working on printing items in receipt

--- Receipt.java
+++ Receipt.java
@@ -4,30 +4,38 @@ public class Receipt{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int items = 0;
-        String order = "";
-        System.out.println("Hello, welcome to Ethan's Epic Edamame!");
+        String[] order = {"", "", "", "", ""};
+        int price = 0;
+        System.out.println("Hello, welcome to Ethan's Epic Edamame! Every item on the menu is $5!");
         while (true) {
-        System.out.print("what would you like to order?: ");
-        String item = scan.nextLine();
-        while (!item.toLowerCase().contains("edamame")) {
-                System.out.print("That Item is not on the menu! Please try again: ");
-                item = scan.nextLine();
+            System.out.print("What would you like to order?: ");
+            String item = scan.nextLine();
+            while (!item.toLowerCase().contains("edamame")) {
+                    System.out.print("That Item is not on the menu! Please try again: ");
+                    item = scan.nextLine();
+                }
+            order[items] += item + ", ";
+            System.out.println(order);
+            items += 1;
+            price += 5;
+            System.out.println(order);
+            if (items == 5) {
+                System.out.println("You have reached the maximum amount of items!");
+                break;
+                }
+            System.out.print("Is that all you would like to order? (y/n): ");
+            String confirmation = scan.nextLine();
+            if (confirmation.toLowerCase().contains("y")) {
+                break;
+                }
             }
-        items += 1;
-        order += item + ", ";
-        System.out.println(order);
-        if (items == 5) {
-            System.out.println("You have reached the maximum amount of items!");
-            break;
-        }
-        System.out.print("Is that all you would like to order? (y/n): ");
-        String confirmation = scan.nextLine();
-        if (confirmation.toLowerCase().contains("y")) {
-            break;
+        for (int i = 0; i < Math.floor(order.toString().length() / 40); i ++) {
+            for (int j = 0; j < 40; j++) {
+                System.out.print(order[j]);
+            }
+            System.out.print("\n");
         }
 
-
-        }
     
     }
 }
