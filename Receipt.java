817e61ec89bcf8d899f7eae8f77aec1ee8170a4b
Ethan Avera <averaeth26@7hills.org>
2023-09-01 12:17:25 -0400

    Began Creating Restaurant

--- Receipt.java
+++ Receipt.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 public class Receipt{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scan.nextLine();
-        System.out.print("Enter your lucky number: ");
-        int num1 = scan.nextInt();
-        System.out.print("Enter your other lucky number: ");
-        int num2 = scan.nextInt();
-        System.out.print("Enter your other other lucky number: ");
-        int num3 = scan.nextInt();
-        System.out.println("Hello, " + name + "! Your Magic number is " + (((num1*num2)+10)*2)%num3 + "!");
+        int items = 0;
+        String order = "";
+        System.out.print("Hello, welcome to Ethan's Epic Edamame, what would you like to order?: ");
+        while (true) {
+        String item = scan.nextLine();
+        while (!item.toLowerCase().contains("edimame")) {
+                System.out.print("That Item is not on the menu! Please try again...");
+            }
+        items += 1;
+        System.out.print("Is that all you would like to order?");
+        }
+    
+        System.out.println("Hello!");
     }
 }
