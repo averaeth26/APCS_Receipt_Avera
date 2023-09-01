dbb178daf1df22c4bd7461998fde563c9ffb2211
Ethan Avera <averaeth26@7hills.org>
2023-09-01 12:31:10 -0400

    Created ordering system

--- Receipt.java
+++ Receipt.java
@@ -5,16 +5,29 @@ public class Receipt{
         Scanner scan = new Scanner(System.in);
         int items = 0;
         String order = "";
-        System.out.print("Hello, welcome to Ethan's Epic Edamame, what would you like to order?: ");
+        System.out.println("Hello, welcome to Ethan's Epic Edamame!");
         while (true) {
+        System.out.print("what would you like to order?: ");
         String item = scan.nextLine();
-        while (!item.toLowerCase().contains("edimame")) {
-                System.out.print("That Item is not on the menu! Please try again...");
+        while (!item.toLowerCase().contains("edamame")) {
+                System.out.print("That Item is not on the menu! Please try again: ");
+                item = scan.nextLine();
             }
         items += 1;
-        System.out.print("Is that all you would like to order?");
+        order += item + ", ";
+        System.out.println(order);
+        if (items == 5) {
+            System.out.println("You have reached the maximum amount of items!");
+            break;
+        }
+        System.out.print("Is that all you would like to order? (y/n): ");
+        String confirmation = scan.nextLine();
+        if (confirmation.toLowerCase().contains("y")) {
+            break;
+        }
+
+
         }
     
-        System.out.println("Hello!");
     }
 }
